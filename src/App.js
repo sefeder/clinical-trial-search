3f654bab56d18f134116b118fb5e31ff5cd2590a
Seth Feder <sethfeder2017@u.northwestern.edu>
2018-09-18 02:27:30 -0500

    major file restructuring and styling update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Saved from "./pages/Saved"
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      results: [],
-      savedTrials: []
-    };
-  }
-
-  updateResults = newResults => {
-    this.setState({results: newResults})
-  }
-  updateSavedTrials = newSavedTrials => {
-    this.setState({savedTrials: newSavedTrials})
-  }
-
-  render(){
-    return(
+const App = () => (
+    
       <Router>
         <div>
-          <Navbar />
-          <Wrapper>
-            <Route exact path="/" render={(props) => (<Home updateAppResults={this.updateResults} updateAppSavedTrials={this.updateSavedTrials} results={this.state.results}  {...props} />)} />
-            <Route exact path="/saved" render={(props) => (<Saved updateAppSavedTrials={this.updateSavedTrials} savedTrials={this.state.savedTrials}  {...props} />)} />
-          </Wrapper>
-          <Footer />
+            <Route exact path="/" component={Home} />
         </div>
       </Router>
     )
-  }
-}
+  
+
 export default App;
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import M from 'materialize-css'
 import "./Result.css";
 
 class Result extends Component {
@@ -8,6 +9,13 @@ class Result extends Component {
             showMore: false,
             saved: false
         };
+    }
+    
+    componentDidMount() {
+        document.addEventListener('DOMContentLoaded', function () {
+            var elems = document.querySelectorAll('.collapsible');
+            M.Collapsible.init(elems, {});
+        });
     }
 
     showMore = event => {
@@ -20,77 +28,81 @@ class Result extends Component {
     submitSave = event => {
         event.preventDefault();
         this.setState({ saved: !this.state.saved })
+        M.toast({html: 'Trial added to report', displayLength: 1500})
+        event.target.classList.add('red')
         this.props.saveTrial(event)
     }
 
     submitRemove = event => {
         event.preventDefault();
         this.setState({ saved: !this.state.saved })
+        M.toast({ html: 'Trial removed from report', displayLength: 1500 })
+        event.target.classList.remove('red')
         this.props.removeTrial(event)
 
     }
 
     render() {
         return (
-            <div className="panel panel-default" info={JSON.stringify(this.props.resultObject)}>
-                <h4>{this.props.resultObject.public_title}</h4>
-                {this.state.showMore ? 
-                    <div className='text-left'>
-                        <h4> 
-                            <b>Status:</b> {this.props.resultObject.status ? this.props.resultObject.status : 'No status provided' }
-                        </h4>
-                        <h4> 
-                            <b>Age Range:</b> {this.props.resultObject.age_range ? this.props.resultObject.age_range.min_age +' - '+ this.props.resultObject.age_range.max_age : 'No age range provided'}
-                        </h4>
-                        <h4>
+            <li>
+                <div className="collapsible-header left-align">
+                    <h6>
+                        {this.props.number + 1}) {this.props.resultObject.public_title}  
+                    </h6>
+                </div>
+                <div className="collapsible-body">
+                    <span className="left-align" info={JSON.stringify(this.props.resultObject)}>
+                        <h6>
+                            <b>Status:</b> {this.props.resultObject.status ? this.props.resultObject.status : 'No status provided'}
+                        </h6>
+                        <h6>
+                            <b>Age Range:</b> {this.props.resultObject.age_range ? this.props.resultObject.age_range.min_age + ' - ' + this.props.resultObject.age_range.max_age : 'No age range provided'}
+                        </h6>
+                        <h6>
                             <b>Target Sample Size:</b> {this.props.resultObject.target_sample_size ? this.props.resultObject.target_sample_size : 'No target sample size provided'}
-                        </h4>
-                        <h4> 
-                            <b>Gender:</b> {this.props.resultObject.gender ? this.props.resultObject.gender : 'No gender provided' }
-                        </h4>
-                        <h4>
-                            <b>Conditions:</b> {!this.props.resultObject.conditions ? 'No conditions provided' : this.props.resultObject.conditions.length === 0 ? 'No conditions provided' : <ul> {this.props.resultObject.conditions.map((a, i) => <li key={i}>{a.name}</li>)} </ul> }
-                        </h4>
-                        <h4>
-                            <b>Interventions:</b> {!this.props.resultObject.interventions ? 'No interventions provided' : this.props.resultObject.interventions.length === 0 ? 'No interventions provided' : <ul> {this.props.resultObject.interventions.map((a, i) => <li key={i}>{a.name}</li>)} </ul> }
-                        </h4>
-                        <h4>
-                            <b>Locations:</b> {!this.props.resultObject.locations ? 'No locations provided' : this.props.resultObject.locations.length === 0 ? 'No locations provided' : <ul> {this.props.resultObject.locations.map((a, i) => <li key={i}>{a.name}</li>)} </ul> }
-                        </h4>
-                        <h4>
-                            <b>Records:</b> {!this.props.resultObject.records ? 'No records provided' : this.props.resultObject.records.length === 0 ? 'No records provided' : <ul> {this.props.resultObject.records.map((a, i) => <li key={i}><a target='_blank' href={a.source_url}>{a.source_url}</a></li>)} </ul> }
-                        </h4>
-                        <h4>
-                            <b>Documents:</b> {!this.props.resultObject.documents ? 'No documents provided' : this.props.resultObject.documents.length === 0 ? 'No documents provided' : <ul> {this.props.resultObject.documents.map((a, i) => <li key={i}>{a.name}</li>)} </ul> }
-                        </h4>
-                        <h4>
+                        </h6>
+                        <h6>
+                            <b>Gender:</b> {this.props.resultObject.gender ? this.props.resultObject.gender : 'No gender provided'}
+                        </h6>
+                        <h6>
+                            <b>Conditions:</b> {!this.props.resultObject.conditions ? 'No conditions provided' : this.props.resultObject.conditions.length === 0 ? 'No conditions provided' : <ul> {this.props.resultObject.conditions.map((a, i) => <li key={i}>{a.name}</li>)} </ul>}
+                        </h6>
+                        <h6>
+                            <b>Interventions:</b> {!this.props.resultObject.interventions ? 'No interventions provided' : this.props.resultObject.interventions.length === 0 ? 'No interventions provided' : <ul> {this.props.resultObject.interventions.map((a, i) => <li key={i}>{a.name}</li>)} </ul>}
+                        </h6>
+                        <h6>
+                            <b>Locations:</b> {!this.props.resultObject.locations ? 'No locations provided' : this.props.resultObject.locations.length === 0 ? 'No locations provided' : <ul> {this.props.resultObject.locations.map((a, i) => <li key={i}>{a.name}</li>)} </ul>}
+                        </h6>
+                        <h6>
+                            <b>Records:</b> {!this.props.resultObject.records ? 'No records provided' : this.props.resultObject.records.length === 0 ? 'No records provided' : <ul> {this.props.resultObject.records.map((a, i) => <li key={i}><a target='_blank' href={a.source_url}>{a.source_url}</a></li>)} </ul>}
+                        </h6>
+                        <h6>
+                            <b>Documents:</b> {!this.props.resultObject.documents ? 'No documents provided' : this.props.resultObject.documents.length === 0 ? 'No documents provided' : <ul> {this.props.resultObject.documents.map((a, i) => <li key={i}>{a.name}</li>)} </ul>}
+                        </h6>
+                        <h6>
                             <b>Summary:</b> {this.props.resultObject.brief_summary ? this.props.resultObject.brief_summary : 'No summary provided'}
-                        </h4>
-                    </div>
-                    : <div></div>}
-                <button
-                    className="btn btn-primary"
-                    onClick={this.showMore}
-                >
-                    {!this.state.showMore ? 'Show More' : 'Show Less'}
-                  </button>
-                {!this.state.saved ?
-                <button
-                    className="btn btn-success"
-                    onClick={this.submitSave}
-                >
-                Save
-                </button> :
-                <button
-                    className="btn btn-danger"
-                    onClick={this.submitRemove}
-                >
-                    Remove from Saved
-                </button>
-                }
-                <br />
-                <br />
-            </div>
+                        </h6>
+                        {!this.state.saved ?
+                            <button
+                                id="saveButton"
+                                className="btn"
+                                onClick={this.submitSave}
+                            >
+                                Add to Report
+                            </button> :
+                            <button
+                                id="saveButton"
+                                className="btn"
+                                onClick={this.submitRemove}
+                            >
+                                Remove from Report
+                            </button>
+                        }
+                    </span>
+                </div>
+                
+            </li>
+           
         )
     }
 
